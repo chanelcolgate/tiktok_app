88039d3497ac3928a1c7c785ce765bedece9f946
chanelcolgate <thanhdatbkdn14@gmail.com>
2024-12-20 22:57:01 +0700

    modified:   models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import tiktok_product
-from . import tiktok_order
-from . import tiktok_order_line
-from . import tiktok_order_stage
+# from . import tiktok_order
+# from . import tiktok_order_line
+# from . import tiktok_order_stage
 from . import tiktok_shop
