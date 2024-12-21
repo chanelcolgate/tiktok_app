bed10657c603126e52476b4301bd45598018d34c
chanelcolgate <thanhdatbkdn14@gmail.com>
2024-12-21 14:13:48 +0700

    modified:   models/tiktok_shop.py

--- models/tiktok_shop.py
+++ models/tiktok_shop.py
@@ -7,10 +7,17 @@ class Shop(models.Model):
     name = fields.Char("Name")
     app_key = fields.Char("App Key")
     app_secret = fields.Char("App Secret")
-    auth_code = fields.Char("Auth Code", password="True")
+    auth_code = fields.Char("Auth Code")
+    auth_code_display = fields.Char("Auth Code", compute="_compute_auth_code_display")
 
     line_ids = fields.One2many(
         "tiktok.shop.line",
         "shop_id",
         string="Shops",
     )
+
+    @api.depends("auth_code")
+    def _compute_auth_code_display(self):
+        for record in self:
+            if record.auth_code:
+                record.auth_code_display = record.auth_code[:10] + "..."
