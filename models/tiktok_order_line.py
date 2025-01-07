d80c0e7dabce7871176529c7090f5aa29dbbe644
chanelcolgate <thanhdatbkdn14@gmail.com>
2025-01-07 10:49:38 +0700

    modified:   views/order_view.xml

--- models/tiktok_order_line.py
+++ models/tiktok_order_line.py
@@ -1,3 +1,6 @@
+import requests
+import base64
+
 from odoo import fields, models, api
 
 class OrderLine(models.Model):
@@ -7,7 +10,15 @@ class OrderLine(models.Model):
     name = fields.Char("Product Name")
     order_line_id = fields.Char("Order Line ID")
     sku_image = fields.Binary("Cover")
+    sku_image_url = fields.Char(string="SKU Image URL")
     currency_id = fields.Many2one("res.currency")
     sale_price = fields.Monetary("Sale Price", "currency_id")
 
     order_id = fields.Many2one("tiktok.order", required=True)
+
+    @api.depends("sku_image_url")
+    def _compute_image_url(self):
+        image = False
+        if self.sku_image_url:
+            image = base64.b64encode(requests.get(self.sku_image_url).content)
+        self.sku_image = image
